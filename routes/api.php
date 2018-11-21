2f9d36fcb42a331fc3ae18e6f3d6c156526d8adc
Jed Darrohn <jed@Jeds-MacBook-Pro-2.local>
2018-11-20 20:06:50 -0700

    Settting up new API routes

--- routes/api.php
+++ routes/api.php
@@ -17,11 +17,37 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+/**
+ * Get a list of all climbs
+ */
 Route::get('/climbs', function (Request $request) {
     return App\Climb::all();
 });
 
+/**
+ * Get a climb by its ID
+ */
 Route::get('/climbs/{id}', function (Request $request, $id) {
     return App\Climb::findOrFail($id);
+});
+
+/**
+ * Create a new climb
+ * 
+ * Returns the new climb as JSON
+ */
+Route::post('/climbs', function (Request $request, $id) {
+    return App\Climb::create($request->all());
+});
+
+/**
+ * Update a Climb
+ * 
+ * Returns the updated climb as JSON
+ */
+Route::post('/climbs/{id}', function (Request $request, $id) {
+    $climb =  App\Climb::findOrFail($id);
+    $climb->fill($request->all())->save();
+    return $climb;
 });
 
