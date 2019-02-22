03b5266602fc90d40fc8d89b4d1ddf36fb4152b2
Jed Darrohn <jed@Jeds-MacBook-Pro-2.local>
2019-02-21 20:18:10 -0700

    Added to global

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        \Barryvdh\Cors\HandleCors::class,            
     ];
 
     /**
@@ -36,7 +37,6 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
-            \Barryvdh\Cors\HandleCors::class,            
         ],
 
         'api' => [
